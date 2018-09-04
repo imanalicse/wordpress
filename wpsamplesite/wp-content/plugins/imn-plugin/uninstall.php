e487542caf5eefcaa7bbf3287704b6e5edb065c5
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-09-04 21:38:26 +0600

    uninstalling

--- wpsamplesite/wp-content/plugins/imn-plugin/uninstall.php
+++ wpsamplesite/wp-content/plugins/imn-plugin/uninstall.php
@@ -9,3 +9,8 @@ defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
 global $wpdb, $wp_version;
 
 $wpdb->query( "DELETE FROM {$wpdb->posts} WHERE post_type IN ( 'book' );" );
+
+$wpdb->query( "DELETE meta FROM {$wpdb->postmeta} meta LEFT JOIN {$wpdb->posts} posts ON posts.ID = meta.post_id WHERE posts.ID IS NULL;" );
+
+// Delete orphan relationships.
+$wpdb->query( "DELETE tr FROM {$wpdb->term_relationships} tr LEFT JOIN {$wpdb->posts} posts ON posts.ID = tr.object_id WHERE posts.ID IS NULL;" );
