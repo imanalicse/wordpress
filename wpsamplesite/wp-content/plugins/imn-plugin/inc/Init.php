c223df45f11b65a9a58565d58807ee1cadefc47c
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-09-07 07:46:24 +0600

    starter plugin structure

--- wpsamplesite/wp-content/plugins/imn-plugin/inc/Init.php
+++ wpsamplesite/wp-content/plugins/imn-plugin/inc/Init.php
@@ -15,7 +15,8 @@ final class Init
     {
         return [
             Pages\Admin::class,
-            Base\Enqueue::class
+            Base\Enqueue::class,
+            Base\SettingsLinks::class
         ];
     }
 
@@ -74,16 +75,10 @@ final class Init
 //        function register()
 //        {
 //
-//            add_filter("plugin_action_links_$this->plugin", array($this, 'settings_link'));
+
 //        }
 //
-//        public function settings_link($links)
-//        {
-//            $settings_link = '<a href="admin.php?page=imn_plugin">Settings</a>';
-//            array_push($links, $settings_link);
-//
-//            return $links;
-//        }
+
 //
 //
 //        function activate()
