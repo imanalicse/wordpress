ed1b9f8a958af9d9b79bcc9f03070b5f38c25759
iman <iman@bitmascot.com>
2018-09-12 10:15:44 +0600

    webalive setting

--- wpsamplesite/wp-content/plugins/webalive-plugin/setting-api/settings.php
+++ wpsamplesite/wp-content/plugins/webalive-plugin/setting-api/settings.php
@@ -6,7 +6,29 @@ function register_my_custom_menu_page()
     // add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
     add_menu_page('Webalive Setting', 'Webalive Setting', 'manage_options', 'webalive_setting', 'webalive_setting_callback', 'dashicons-welcome-widgets-menus', 90);
 
+    add_options_page(
+        __( 'My Options', 'textdomain' ),
+        __( 'My Plugin', 'textdomain' ),
+        'manage_options',
+        'my-plugin.php',
+        'my_plugin_page'
+    );
+
     add_action("admin_init", "webalive_custom_settings");
+}
+
+function my_plugin_page() {
+    ?>
+    <?php //settings_errors(); ?>
+
+    <form method="post" action="options.php">
+        <?php
+        settings_fields('webalive-setting-area');
+        do_settings_sections("my-plugin.php");
+        submit_button();
+        ?>
+    </form>
+    <?php
 }
 
 add_action('admin_menu', 'register_my_custom_menu_page');
@@ -21,6 +43,11 @@ function webalive_custom_settings()
 
     add_settings_field('sidebar-name', 'Full Name', 'webalive_sidebar_name', 'webalive_setting', 'webalive-sidebar-options');
     add_settings_field('sidebar-twitter', 'Twitter handler', 'webalive_sidebar_twitter', 'webalive_setting', 'webalive-sidebar-options');
+
+    // Under setting
+    register_setting("webalive-setting-area", "public_key");
+    add_settings_section("webalive-setting-section-area", "Webalive options", "webalive_setting_area", "my-plugin.php");
+    add_settings_field("public-key-handler", "Public Key", "public_key_callback", "my-plugin.php", "webalive-setting-section-area");
 }
 
 function webalive_sidebar_options()
@@ -54,4 +81,14 @@ function webalive_sanitize_twitter_handler($input)
 function webalive_setting_callback()
 {
     require_once "template.php";
+}
+
+// under setting
+function webalive_setting_area() {
+    echo "Key Info";
+}
+
+function public_key_callback() {
+    $public_key = esc_attr(get_option('public_key'));
+    echo '<input type="text" name="public_key" value="' . $public_key . '" placeholder="Public Key">';
 }
