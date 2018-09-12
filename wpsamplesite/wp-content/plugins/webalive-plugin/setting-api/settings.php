0a8c7f8793431767ddd6d9c97db2bcfc5f6038f8
iman <iman@bitmascot.com>
2018-09-12 12:29:00 +0600

    wp setting db optimization

--- wpsamplesite/wp-content/plugins/webalive-plugin/setting-api/settings.php
+++ wpsamplesite/wp-content/plugins/webalive-plugin/setting-api/settings.php
@@ -7,8 +7,8 @@ function register_my_custom_menu_page()
     add_menu_page('Webalive Setting', 'Webalive Setting', 'manage_options', 'webalive_setting', 'webalive_setting_callback', 'dashicons-welcome-widgets-menus', 90);
 
     add_options_page(
-        __( 'My Options', 'textdomain' ),
-        __( 'My Plugin', 'textdomain' ),
+        __('My Options', 'textdomain'),
+        __('My Plugin', 'textdomain'),
         'manage_options',
         'my-plugin.php',
         'my_plugin_page'
@@ -17,9 +17,11 @@ function register_my_custom_menu_page()
     add_action("admin_init", "webalive_custom_settings");
 }
 
-function my_plugin_page() {
+function my_plugin_page()
+{
     ?>
-    <?php //settings_errors(); ?>
+    <?php //settings_errors();
+    ?>
 
     <form method="post" action="options.php">
         <?php
@@ -35,9 +37,11 @@ add_action('admin_menu', 'register_my_custom_menu_page');
 
 function webalive_custom_settings()
 {
-    register_setting("webalive-settings-group", "first_name");
-    register_setting("webalive-settings-group", "last_name");
-    register_setting("webalive-settings-group", "twitter_handler", 'webalive_sanitize_twitter_handler');
+//    register_setting("webalive-settings-group", "first_name");
+//    register_setting("webalive-settings-group", "last_name");
+//    register_setting("webalive-settings-group", "twitter_handler", 'webalive_sanitize_twitter_handler');
+
+    register_setting("webalive-settings-group", "webalive_plugin_setting");
 
     add_settings_section('webalive-sidebar-options', 'Sidebar Options', 'webalive_sidebar_options', 'webalive_setting');
 
@@ -57,18 +61,20 @@ function webalive_sidebar_options()
 
 function webalive_sidebar_name()
 {
-    $firstName = esc_attr(get_option('first_name'));
-    $lastName = esc_attr(get_option('last_name'));
+    $options = get_option('webalive_plugin_setting');
+    $firstName = esc_attr($options['first_name']);
+    $lastName = esc_attr($options['last_name']);
 
-    echo '<input type="text" name="first_name" value="' . $firstName . '" placeholder="First Name">';
-    echo '<input type="text" name="last_name" value="' . $lastName . '" placeholder="Last Name">';
+    echo '<input type="text" name="webalive_plugin_setting[first_name]" value="' . $firstName . '" placeholder="First Name">';
+    echo '<input type="text" name="webalive_plugin_setting[last_name]" value="' . $lastName . '" placeholder="Last Name">';
 }
 
 
 function webalive_sidebar_twitter()
 {
-    $twitter = esc_attr(get_option('twitter_handler'));
-    echo '<input type="text" name="twitter_handler" value="' . $twitter . '" placeholder="Twitter Handler"><p class="description">Input your twitter username without the @ character</p>';
+    $options = get_option('webalive_plugin_setting');
+    $twitter = esc_attr($options['twitter_handler']);
+    echo '<input type="text" name="webalive_plugin_setting[twitter_handler]" value="' . $twitter . '" placeholder="Twitter Handler"><p class="description">Input your twitter username without the @ character</p>';
 }
 
 function webalive_sanitize_twitter_handler($input)
@@ -84,11 +90,13 @@ function webalive_setting_callback()
 }
 
 // under setting
-function webalive_setting_area() {
+function webalive_setting_area()
+{
     echo "Key Info";
 }
 
-function public_key_callback() {
+function public_key_callback()
+{
     $public_key = esc_attr(get_option('public_key'));
     echo '<input type="text" name="public_key" value="' . $public_key . '" placeholder="Public Key">';
 }
