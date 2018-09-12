9b5f95cdb0e9c3ee06605a5ee23fd06a1acde37b
iman <iman@bitmascot.com>
2018-09-12 14:46:05 +0600

    post type

--- wpsamplesite/wp-content/plugins/webalive-plugin/webalive-plugin.php
+++ wpsamplesite/wp-content/plugins/webalive-plugin/webalive-plugin.php
@@ -17,34 +17,4 @@ if (!defined('ABSPATH')) {
 
 require_once "setting-api/settings.php";
 
-add_action('init', 'custom_post_type');
-
-function custom_post_type()
-{
-    register_taxonomy(
-        'portfolio_category',
-        'portfolio',
-        array(
-            'name' => __( 'Categories', 'taxonomy general name'),
-            'singular_name' => __( 'Category', 'taxonomy singular name'),
-            'all_items' => __( 'All Categories'),
-            'edit_item' => __('Edit Category' ),
-            'query_var' => true,
-            'hierarchical' => true,
-            'rewrite' => true
-        )
-    );
-
-    register_post_type('portfolio', array(
-        'labels' => array(
-            'name' => __('Portfolios'),
-            'singular_name' => __('Portfolio'),
-            'add_new_item' => __('Add New', 'textdomain'),
-            'edit_item' => __('Edit', 'textdomain'),
-        ),
-        'public' => true,
-        'capability_type' => 'post',
-        'supports' => array('title', 'editor', 'thumbnail', 'page-attributes'),
-        'has_archive' => true
-    ));
-}
+require_once "post-type.php";
