a29e3b00b3f5707755a0d1dd7caf8fddf0631f01
iman <iman@bitmascot.com>
2018-09-12 14:43:55 +0600

    taxonomy

--- wpsamplesite/wp-content/plugins/webalive-plugin/webalive-plugin.php
+++ wpsamplesite/wp-content/plugins/webalive-plugin/webalive-plugin.php
@@ -21,10 +21,24 @@ add_action('init', 'custom_post_type');
 
 function custom_post_type()
 {
-    register_post_type('testimonial', array(
+    register_taxonomy(
+        'portfolio_category',
+        'portfolio',
+        array(
+            'name' => __( 'Categories', 'taxonomy general name'),
+            'singular_name' => __( 'Category', 'taxonomy singular name'),
+            'all_items' => __( 'All Categories'),
+            'edit_item' => __('Edit Category' ),
+            'query_var' => true,
+            'hierarchical' => true,
+            'rewrite' => true
+        )
+    );
+
+    register_post_type('portfolio', array(
         'labels' => array(
-            'name' => __('Testimonials'),
-            'singular_name' => __('Testimonial'),
+            'name' => __('Portfolios'),
+            'singular_name' => __('Portfolio'),
             'add_new_item' => __('Add New', 'textdomain'),
             'edit_item' => __('Edit', 'textdomain'),
         ),
