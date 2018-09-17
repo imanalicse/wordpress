a6eec31f3f33bd600971c540bf80ee352cbb06d2
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-09-17 21:28:28 +0600

    imn sample

---
+++ wpsamplesite/wp-content/plugins/imn-sample/imn-sample.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package  ImnPlugin
+ */
+/*
+Plugin Name: Imn Sample
+Plugin URI: http://imn.com/plugin
+Description: This is my first attempt on writing a custom Plugin for this amazing tutorial series.
+Version: 1.0.0
+Author: IMN
+Author URI: http://imn.com
+License: GPLv2 or later
+Text Domain: imn-plugin
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
---
+++ wpsamplesite/wp-content/plugins/imn-sample/uninstall.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Trigger this file on Plugin uninstall
+ *
+ * @package  ImnPlugin
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die;
+}
+
+// Clear Database stored data
