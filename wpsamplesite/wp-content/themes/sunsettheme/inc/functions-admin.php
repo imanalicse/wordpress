7e5ca8d964902b85787830d083c0a1d74590ee40
iman <iman@bitmascot.com>
2018-09-19 17:17:22 +0600

    custom opionts

--- wpsamplesite/wp-content/themes/sunsettheme/inc/functions-admin.php
+++ wpsamplesite/wp-content/themes/sunsettheme/inc/functions-admin.php
@@ -6,7 +6,8 @@ ADMIN PAGE
 ================
 */
 
-function sunset_add_admin_page() {
+function sunset_add_admin_page()
+{
     // Generate Sunset Admin page
     add_menu_page('Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page', 'dashicons-admin-customizer', 110);
 
@@ -15,15 +16,40 @@ function sunset_add_admin_page() {
     add_submenu_page('alecaddd_sunset', 'Sunset Theme Options', 'General', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page');
 
     add_submenu_page('alecaddd_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'alecaddd_sunset_css', 'subset_theme_setting_page');
+
+    // Activate custom setting
+    add_action('admin_init', 'sunset_custom_settings');
 }
 
 add_action('admin_menu', 'sunset_add_admin_page');
 
-function sunset_theme_create_page() {
-
-    require_once( get_template_directory().'/inc/templates/sunset-admin.php');
+function sunset_custom_settings()
+{
+    register_setting("sunset-settings-group", 'first_name');
+    add_settings_section('sunset-sidebar-options', 'Sidebar Options', 'sunset_sidebar_options', 'alecaddd_sunset');
+    add_settings_field('sidebar-name', 'First Name', 'sunset_sidebar_name', 'alecaddd_sunset', 'sunset-sidebar-options');
 }
 
-function subset_theme_setting_page() {
+function sunset_sidebar_options()
+{
+    echo "Customize your sidebar information";
+}
+
+function sunset_sidebar_name()
+{
+    $firstName = esc_attr(get_option('first_name'));
+
+    echo '<input type="text" name="first_name" value="' . $firstName . '" placeholder="First Name"/>';
+}
+
+
+function sunset_theme_create_page()
+{
+
+    require_once(get_template_directory() . '/inc/templates/sunset-admin.php');
+}
+
+function subset_theme_setting_page()
+{
 
 }
