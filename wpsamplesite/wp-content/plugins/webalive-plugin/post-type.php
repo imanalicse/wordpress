21a756ee16a9f4393d7d8eb1542b6983a291e9ff
iman <iman@bitmascot.com>
2018-09-24 14:56:32 +0600

    register texonomy

--- wpsamplesite/wp-content/plugins/webalive-plugin/post-type.php
+++ wpsamplesite/wp-content/plugins/webalive-plugin/post-type.php
@@ -18,6 +18,35 @@ function custom_post_type()
         )
     );
 
+    $labels = array(
+        'name'              => _x( 'Genres', 'taxonomy general name', 'textdomain' ),
+        'singular_name'     => _x( 'Genre', 'taxonomy singular name', 'textdomain' ),
+        'search_items'      => __( 'Search Genres', 'textdomain' ),
+        'all_items'         => __( 'All Genres', 'textdomain' ),
+        'parent_item'       => __( 'Parent Genre', 'textdomain' ),
+        'parent_item_colon' => __( 'Parent Genre:', 'textdomain' ),
+        'edit_item'         => __( 'Edit Genre', 'textdomain' ),
+        'update_item'       => __( 'Update Genre', 'textdomain' ),
+        'add_new_item'      => __( 'Add New Genre', 'textdomain' ),
+        'new_item_name'     => __( 'New Genre Name', 'textdomain' ),
+        'menu_name'         => __( 'Genre', 'textdomain' ),
+    );
+
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'genre' ),
+    );
+
+    register_taxonomy(
+        'portfolio_genre',
+        'portfolio',
+        $args
+    );
+
     register_post_type('portfolio', array(
         'labels' => array(
             'name' => __('Portfolios'),
