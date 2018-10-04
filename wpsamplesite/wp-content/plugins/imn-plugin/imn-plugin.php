ec858f0457dc135c64af5c7ffa2ec93ad0e96bcc
iman <iman@bitmascot.com>
2018-10-04 12:54:47 +0600

    Autoload psr 4

--- wpsamplesite/wp-content/plugins/imn-plugin/imn-plugin.php
+++ wpsamplesite/wp-content/plugins/imn-plugin/imn-plugin.php
@@ -7,7 +7,7 @@ Plugin Name: Imn Plugin
 Plugin URI: https://imanalicse.github.io/imn-plugin
 Description: This is a sample plugin for repeated uses resources
 Version: 1.0.0
-Author: Iman
+Author: Webalive
 Author URI: https://imanalicse.github.io
 License: GPLv2 or later
 Text Domain: imn-plugin
@@ -16,3 +16,29 @@ Text Domain: imn-plugin
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
+
+define("IMN_PLUGIN_PATH", plugin_dir_path(__FILE__));
+
+if(file_exists(IMN_PLUGIN_PATH . './vendor/autoload.php')) {
+    require_once IMN_PLUGIN_PATH . './vendor/autoload.php';
+}
+
+use Inc\Admin\Pages;
+Pages::register();
+
+
+/**
+ * The code that runs during plugin activation
+ */
+function activate_imn_plugin() {
+    Inc\Base\Activate::activate();
+}
+register_activation_hook( __FILE__, 'activate_imn_plugin' );
+
+/**
+ * The code that runs during plugin deactivation
+ */
+function deactivate_imn_plugin() {
+    Inc\Base\Deactivate::deactivate();
+}
+register_deactivation_hook( __FILE__, 'deactivate_imn_plugin' );
---
+++ wpsamplesite/wp-content/plugins/imn-plugin/inc/admin/Pages.php
@@ -0,0 +1,14 @@
+<?php
+namespace Inc\Admin;
+
+class Pages {
+    
+    public function __construct()
+    {
+        echo 'aaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+    }
+
+    public static function register(){
+        echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    }
+}
