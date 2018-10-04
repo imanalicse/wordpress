b8af265b7367792b485a8894d5da91885be7a676
iman <iman@bitmascot.com>
2018-10-04 10:57:50 +0600

    Imn pluing start

---
+++ wpsamplesite/wp-content/plugins/imn-plugin/imn-plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package  ImnPlugin
+ */
+/*
+Plugin Name: Imn Plugin
+Plugin URI: https://imanalicse.github.io/imn-plugin
+Description: This is a sample plugin for repeated uses resources
+Version: 1.0.0
+Author: Iman
+Author URI: https://imanalicse.github.io
+License: GPLv2 or later
+Text Domain: imn-plugin
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
